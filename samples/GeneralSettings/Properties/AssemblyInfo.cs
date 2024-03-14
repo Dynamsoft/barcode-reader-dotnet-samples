ea56559c29a5a8b6d003cf46d34ff37aea860410
DMGithubPublisher <github@dynamsoft.com>
2024-03-14 17:10:33 +0800

    update to v9.6.40

--- samples/GeneralSettings/Properties/AssemblyInfo.cs
+++ samples/GeneralSettings/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GeneralSettings")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- samples/UseCases/ProcessDocumentsByBarcodes/Properties/AssemblyInfo.cs
+++ samples/UseCases/ProcessDocumentsByBarcodes/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dynamsoft")]
 [assembly: AssemblyProduct("ProcessDocumentsByBarcodes")]
-[assembly: AssemblyCopyright("Copyright © Dynamsoft Corporation 2016")]
+[assembly: AssemblyCopyright("Copyright © Dynamsoft Corporation 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
